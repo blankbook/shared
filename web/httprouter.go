d0f1897de6d81154f1f16a278630d1bb83d9e12c
Jacob Brunsting <jacob.brunsting@gmail.com>
2017-06-07 12:35:11 -0400

    Fix routing setup, and add the ability to strip a path prefix from every request

--- web/httprouter.go
+++ web/httprouter.go
@@ -1,21 +1,42 @@
 package web
 
 import (
-    "github.com/gorilla/mux"
+    "fmt"
     "net/http"
+
+    "github.com/gorilla/mux"
 )
 
 type HttpRouter struct {
     router *mux.Router
+    pathPrefix string
 }
 
-func NewHttpRouter(router *mux.Router) *HttpRouter {
-    return &HttpRouter{router}
+func NewHttpRouter(router *mux.Router, pathPrefix string) *HttpRouter {
+    return &HttpRouter{router, pathPrefix}
 }
 
 func (r *HttpRouter) HandleRoute(method int, path string,
                                 handler func(http.ResponseWriter,
                                              *http.Request)) {
+    var methodStr string
+    switch method {
+    case GET:
+        methodStr = "GET"
+    case POST:
+        methodStr = "POST"
+    case PUT:
+        methodStr = "PUT"
+    case DELETE:
+        methodStr = "DELETE"
+    }
+    r.router.HandleFunc(path, handler).Methods(methodStr)
+}
 
-   r.router.HandleFunc(path, handler).Methods(path).Schemes("http")
+func (r *HttpRouter) StartListening() {
+    http.Handle("/", http.StripPrefix(r.pathPrefix, r.router))
+}
+
+func TestHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Testing!!")
 }
