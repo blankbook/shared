baf9d50c1e211830fe3207e4f7445ce4d28e7563
Jacob Brunsting <jacob.brunsting@gmail.com>
2017-06-07 09:51:00 -0400

    Add models to shared resources

---
+++ models/model.go
@@ -0,0 +1,9 @@
+// Package model provides an interface for all models, ensuring all models 
+// have validation functions of the same format
+package models
+
+// Model is an interface for the models of the data from incoming requests
+type Model interface {
+    // validate 
+    Validate() (bool, []string)
+}
