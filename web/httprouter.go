f5d5b0b384779dda4e8bdd2eb23cb7fd0289f953
Jacob Brunsting <jacob.brunsting@gmail.com>
2017-06-07 09:43:27 -0400

    Finish creating HttpRouter for routing API calls

--- web/httprouter.go
+++ web/httprouter.go
@@ -2,16 +2,20 @@ package web
 
 import (
     "github.com/gorilla/mux"
+    "net/http"
 )
 
 type HttpRouter struct {
     router mux.Router
 }
 
-func NewHttpRouter(router mux.Router) {
-   // gotta do stuf here, use mux to set path prefixes
+func NewHttpRouter(router mux.Router) *HttpRouter {
+    return &HttpRouter{router}
 }
 
-func (r HttpRouter) HandleRoute(method int, path string, handler func(http.ResponseWriter, *http.Request)) {
-   r.router.... 
+func (r *HttpRouter) HandleRoute(method int, path string,
+                                handler func(http.ResponseWriter,
+                                             *http.Request)) {
+
+   r.router.HandleFunc(path, handler).Methods(path).Schemes("http")
 }
